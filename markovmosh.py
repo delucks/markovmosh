62beb08dfbb9782ec96117c641c1549389ca2c8f
delucks <jamie.delucks@gmail.com>
2015-02-08 01:42:59 -0500

    Cleaned up key generation

--- markovmosh.py
+++ markovmosh.py
@@ -8,32 +8,28 @@ import argparse
 # based on coleifer's irc bot. will be modifying moar
 
 class Markov():
-    chain_length = 2
-    max_words = 30
     separator = '\x01'
     stop_word = '\x02'
-    messages_to_generate = 100
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, chain_length = 2, max_words = 30, gen_limit = 100):
         self.redis_conn = redis.Redis()
+        self.chain_length = chain_length
+        self.max_words = max_words
+        self.gen_limit = gen_limit
     
     def make_key(self, k):
-        return '-'.join(k)
+        return self.separator.join(k)
     
+    # TODO: Can use some more things to sanitize out, maybe code and markup
     def sanitize_message(self, message):
-        return re.sub('[\"\']', '', message.lower())
+        return re.sub('[\"\',]', '', message.lower())
 
     def split_message(self, message):
         # split the incoming message into words, i.e. ['what', 'up', 'bro']
         words = message.split()
         # if the message is any shorter, it won't lead anywhere
         if len(words) > self.chain_length:
-            # add some stop words onto the message
-            # ['what', 'up', 'bro', '\x02']
             words.append(self.stop_word) 
-            # len(words) == 4, so range(4-2) == range(2) == 0, 1, meaning
-            # we return the following slices: [0:3], [1:4]
-            # or ['what', 'up', 'bro'], ['up', 'bro', '\x02']
             for i in range(len(words) - self.chain_length):
                 yield words[i:i + self.chain_length + 1]
     
@@ -50,23 +46,24 @@ class Markov():
         return ' '.join(gen_words)
 
     def read(self,f):
+      # read in an open fd, add all lines to the redis store
       for line in f:
         for words in self.split_message(self.sanitize_message(line)):
             # grab everything but the last word
-            key = self.separator.join(words[:-1])
+            key = words[:-1]
             # add the last word to the set
             self.redis_conn.sadd(self.make_key(key), words[-1])
 
     def generate(self,key):
+      # iterate through gen_limit rounds, pick a random choice from long ones
       best_message = ''
       messages = []
-      for i in range(self.messages_to_generate):
+      for i in range(self.gen_limit):
         generated = self.generate_message(seed=key)
         if len(generated) > len(best_message):
           best_message = generated
         if best_message:
           messages.append(best_message)
-
       if len(messages):
         return random.choice(messages)
 
@@ -129,7 +126,6 @@ class Markov():
 #p.add_argument("-n", "--number", type=int, help="number of posts you want (be reasonable). defaults to 100", default=100)
 #args = p.parse_args()
 
-
 m = Markov()
-#m.setup()
-m.go(sys.argv[1])
+m.setup()
+#m.go(sys.argv[1])
